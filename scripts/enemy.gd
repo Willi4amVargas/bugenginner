8aad0fa9e5bf4e2891832d380182c2f4b65e3a2b
Willi4amVargas <williamvargas0907@gmail.com>
2025-03-26 19:11:57 -0400

    v1.0.1.0 añadido del nodo del enemigo y parte de la logica para la batalla

---
+++ scripts/enemy.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body.name=="Player"):
+		get_tree().change_scene_to_file("res://scenes/battle_scene_1.tscn")
---
+++ scripts/game_configs.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var player_stadistics={
+	"life":10,
+	"attack":4
+}
+var enemy_stadistics = {
+	"life":5,
+	"attack":1
+}
